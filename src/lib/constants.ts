fa828a4808dea1d2e68492df5f5d63e99c7e610d
Sendofr <elverdaderojosemartinez@gmail.com>
2026-06-10 15:03:49 -0300

    Updated constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,7 +4,6 @@ export const SERVICES: { name: ServiceType; price: number }[] = [
   { name: 'Corte', price: 40000 },
   { name: 'Barba', price: 25000 },
   { name: 'Combo', price: 60000 },
-  { name: 'Afeitado', price: 2500 },
   { name: 'Cejas', price: 10000 },
   { name: 'Lavado', price: 10000 },
 ];
