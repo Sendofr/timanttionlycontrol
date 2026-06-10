44ec68319f44a1d86c4bd234878266cd311b7f24
Sendofr <elverdaderojosemartinez@gmail.com>
2026-06-10 15:02:43 -0300

    Updated constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,7 @@ import type { ServiceType, PaymentMethod } from '../types';
 
 export const SERVICES: { name: ServiceType; price: number }[] = [
   { name: 'Corte', price: 40000 },
-  { name: 'Barba', price: 10000 },
+  { name: 'Barba', price: 25000 },
   { name: 'Combo', price: 60000 },
   { name: 'Afeitado', price: 2500 },
   { name: 'Cejas', price: 10000 },
